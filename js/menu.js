8cb960f1a0c2200fbd1b16dd40b1cfa5140b9f35
marcinstenka <44028611+marcinstenka@users.noreply.github.com>
2019-03-22 21:00:29 +0100

    initial

---
+++ js/menu.js
@@ -0,0 +1,36 @@
+function openMenu() {
+    document.getElementById("side-menu").style.width = '250px';
+    
+    document.getElementById("menu_js").style.marginRight = '250px';
+    document.getElementById("menu_js").style.marginLeft = '-250px';
+}
+function closeMenu() {
+    document.getElementById("side-menu").style.width = '0';
+    
+    document.getElementById("menu_js").style.marginRight = '0';
+    document.getElementById("menu_js").style.marginLeft = '0';
+}
+$(function(){
+	var navbar = $('.nav');
+    var icon = $('.menu-icon');
+	var dropdown = $('#menudrop');
+    
+	$(window).scroll(function(){
+		if($(window).scrollTop() <= 40){
+			navbar.removeClass('scroll');
+            icon.addClass('menu-icon-scroll');
+            dropdown.removeClass('dropdown');
+            var oldSrc = 'img/logoC.png';
+            var newSrc = 'img/logoB.png';
+            $('#logo_topbar img[src="' + oldSrc + '"]').attr('src', newSrc);
+            
+		} else {
+			navbar.addClass('scroll');
+            icon.removeClass('menu-icon-scroll');
+            dropdown.addClass('dropdown');
+            var oldSrc = 'img/logoB.png';
+            var newSrc = 'img/logoC.png';
+            $('#logo_topbar img[src="' + oldSrc + '"]').attr('src', newSrc);
+		}
+	});
+});
